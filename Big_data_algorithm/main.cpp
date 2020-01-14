e60d1a6b2a5c5004a71862661e8977c657ab3df4
vancasola <616923882@qq.com>
2020-01-14 19:59:55 +0800

    min

--- Big_data_algorithm/main.cpp
+++ Big_data_algorithm/main.cpp
@@ -2,14 +2,33 @@
 //  main.cpp
 //  Big_data_algorithm
 //
-//  Created by 周政演 on 2020/1/14.
+//  Created by vancasola on 2020/1/14.
 //  Copyright © 2020 none. All rights reserved.
-//
+// 
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <cstdio>
+#include <ctime>
+#include <cmath>
+using namespace std;
+int main(){
+    time_t ti;
+    double epsilon=0.1,delta=0.25,tmp,sum=0;
+    
+    double t=1/(delta*epsilon*epsilon),m,n=100000;
+    for(int i=0;i<t;i++){
+        srand((unsigned)time(&ti)+i);
+        m=1.0;
+        for(int j=0;j<n;j++){
+            tmp=rand()/(RAND_MAX+1.0);
+            if(tmp<m)m=tmp;
+        }
+        sum+=m;
+    }
+    m=sum/t;
+    printf("epsilon:%.3lf delta:%.3lf\n",epsilon,delta);
+    printf("t==%lf\n",t);
+    printf("ground:%.1lf estimate:%.1lf\n",n,1.0/m+1);
+    printf("error:%.2lf%%",fabs((n-(1.0/m+1))/n)*100);
     return 0;
 }
