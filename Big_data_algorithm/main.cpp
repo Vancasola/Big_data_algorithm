8455b0a1a69e85ce409c616f430f83fdd3d2ce2d
vancasola <616923882@qq.com>
2020-01-14 19:14:56 +0800

    Initial Commit

---
+++ Big_data_algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Big_data_algorithm
+//
+//  Created by 周政演 on 2020/1/14.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
